449797286359ba4bae45b8d871d3e598f4b6f896
Maya <mayabettzy@gmail.com>
2024-04-26 20:49:41 -0400

    Revisión de entrada de datos, operadores y operadores booleanos

---
+++ datos_de_entrada.py
@@ -0,0 +1,10 @@
+mi_variable = input("Escribe tu valor: ")
+print("tu valor es: " + mi_variable)
+
+a = input("Primer número: ")
+b = input("Segundo número: ")
+c = int(input("Tercer número: "))
+a = int(a)
+b = int(b)
+resultado = a + b + c
+print("resultado: " + str(resultado))
